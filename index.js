1eda967a442455686e37748505bf9208663df812
gh120515 <123872197+gh120515@users.noreply.github.com>
2023-05-21 20:14:31 +0930

    refactored index.js

--- index.js
+++ index.js
@@ -2,12 +2,10 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 const { Circle, Square, Triangle } = require('./lib/shapes')
-// file to export logo into
-const file = './logo.svg';
+const util = require('util');
 
 // user prompts
-async function getUserInput() {
-    const userInput = await inquirer.prompt([
+const questions = ([
       {
         type: 'input',
         name: 'text',
@@ -33,61 +31,66 @@ async function getUserInput() {
         name: 'shapeColour',
         message: 'Enter the shape color (name [e.g. white]  or HEX colour code):',
       },
-    ]);
-  
-    return userInput;
-  };
+]);
 
-// generate logo function
-function generateLogo() {
-    
-    // function to set logo components, based on shape chosen
-    let logoShape = function(answer) {
-        // create new objects from imported Shapes classes
-        if (answer.shape === 'circle') {
-            let userShape = new Circle (
-                answer.text,
-                answer.textColour,
-                answer.shapeColour
-            )
-            return userShape.render();
-        }
-
-        if (answer.shape === 'square') {
-            let userShape = new Square (
-                answer.text,
-                answer.textColour,
-                answer.shapeColour
-            )
-            return userShape.render();
-        }
-
-        if (answer.shape === 'triangle') {
-            let userShape = new Triangle (
-                answer.text,
-                answer.textColour,
-                answer.shapeColour
-            )
-            return userShape.render();
-        }
+ // function to set logo components, based on shape chosen
+ function logoShape(answer) {
+    // create new objects from imported Shapes classes
+    if (answer.shape === 'circle') {
+        let userShape = new Circle (
+            answer.text,
+            answer.textColour,
+            answer.shapeColour
+        )
+        return userShape.render();
     }
 
-    // write to file using fs
-    fs.writeFile(file, logoShape,
+    if (answer.shape === 'square') {
+        let userShape = new Square (
+            answer.text,
+            answer.textColour,
+            answer.shapeColour
+        )
+        return userShape.render();
+    }
+
+    if (answer.shape === 'triangle') {
+        let userShape = new Triangle (
+            answer.text,
+            answer.textColour,
+            answer.shapeColour
+        )
+        return userShape.render();
+    }
+};
+
+// fs to write SVG file
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data,
         err => {
             if(err) {
                 console.log(err)
             }
             console.log('Your logo is ready!')
-        })
-};
+        }
+    )};
 
 
+// util package to use await on iquirer prompts & SVG file creation
+const createFile = util.promisify(writeToFile)
 // run app
 async function init() {
-    const userInput = await getUserInput();
-    // generate logo based on user input
-    generateLogo(userInput);
-  }
-  
+    // try & catch to log error on failure
+    try {
+    // initiate the inquirer module
+    const answers = await inquirer.prompt(questions);
+    // generate the SVG logo file
+    const generate = logoShape(answers)
+    await createFile('logo.svg', generate);
+    } catch (err) {
+        console.log(err);
+    }
+};
+
+// Function call to initialize app
 init();
